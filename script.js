7b869dd6a68e046381d25027697d2bef63754340
Willams S. de Sousa <osdeving@gmail.com>
2024-09-17 04:40:03 -0300

    feat: finalizar o projeto

--- script.js
+++
@@ -1,7 +0,0 @@
-const checkboxInput = document.querySelector('.checkbox-input');
-const checkboxCustomizado = document.querySelector('.checkbox-customizado');
-
-checkboxCustomizado.addEventListener('click', function() {
-    checkboxInput.checked = !checkboxInput.checked;
-    checkboxCustomizado.classList.toggle('checked');
-});
